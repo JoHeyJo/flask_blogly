c2df661d5d8ee524725b4e2f7245f20ee8d23e84
jf <jpf0628@gmail.com>
2022-03-30 15:32:24 -0700

    finished user interface

---
+++ app.py
@@ -0,0 +1,19 @@
+"""Blogly application."""
+
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'oh-so-secret'
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+@app.get("'")
+def show_home()
